5c7304b7ff15f82b29f7a9e02f618c53907ea080
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 07:10:35 +0100

    Implement graceful shutdown

--- internal/http/server/server.go
+++ internal/http/server/server.go
@@ -9,12 +9,17 @@ import (
 
 var s http.Server
 
+func Shutdown(ctx context.Context) error {
+	log.Println("server.Shutdown shutting down")
+	return s.Shutdown(ctx)
+}
+
 func Run(ctx context.Context, senv *ServerEnv) {
 	s.Addr = net.JoinHostPort("", senv.Port)
 	log.Printf("server.Run starting server on %s", s.Addr)
 
 	err := http.ListenAndServe(s.Addr, s.Handler)
 	if err != nil {
-		panic(err)
+		log.Fatal("server.Run ", err)
 	}
 }
