749b552dbdd0b1ed3ad312c9b2233b1f8c0e0233
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 06:22:12 +0100

    Set port flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	flag "github.com/spf13/pflag"
+)
+
+//flags
+var (
+	port string
+)
+
+func main() {
+	parseFlags()
+	log.Printf("main.Main Port: %s", port)
+}
+
+func parseFlags() {
+	flag.StringVarP(&port, "port", "p", "8080", "port to run server on")
+
+	flag.Parse()
 }
