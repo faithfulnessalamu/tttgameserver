ceb59be154fdcebfdc1105cfc40d54091214b1be
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 09:46:54 +0100

    Init a game engine in each handler

--- internal/http/handler/joingamehandler.go
+++ internal/http/handler/joingamehandler.go
@@ -3,10 +3,13 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/thealamu/internal/engine"
 )
 
 //JoinGameHandler handles game joining
 func JoinGameHandler() http.HandlerFunc {
+	gE := engine.New()
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Join Game")
 	}
--- internal/http/handler/newgamehandler.go
+++ internal/http/handler/newgamehandler.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"github.com/thealamu/internal/engine"
 )
 
 //NewGameHandler handles creating a new game
 func NewGameHandler() http.HandlerFunc {
+	gE := engine.New()
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "New Game")
 	}
