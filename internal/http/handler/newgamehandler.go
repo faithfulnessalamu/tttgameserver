2cf191431a352fc40cfaeb7550832aefed0a099f
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 12:20:18 +0100

    Inject db dependency into each handler

--- internal/http/handler/newgamehandler.go
+++ internal/http/handler/newgamehandler.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/patrickmn/go-cache"
 	"github.com/thealamu/tttgameserver/internal/engine"
 )
 
 //NewGameHandler handles creating a new game
-func NewGameHandler() http.HandlerFunc {
-	gE := engine.New()
+func NewGameHandler(db *cache.Cache) http.HandlerFunc {
+	gE := engine.New(db)
 	return func(w http.ResponseWriter, r *http.Request) {
 		gameID := gE.StartNewGame()
 		log.Printf("handler.NewGame gameID is %s", gameID)
