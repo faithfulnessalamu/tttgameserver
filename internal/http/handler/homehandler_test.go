b285c59ebc3eb0d66a3f32da2e81a66845520790
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 08:16:29 +0100

    Add test for home handler

---
+++ internal/http/handler/homehandler_test.go
@@ -0,0 +1,29 @@
+package handler
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestHomeHandler(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+
+	router.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Bad status code, got: %v, want: %v", rr.Code, http.StatusOK)
+	}
+	data := rr.Body.String()
+	if data != "TTTGameServer\n" {
+		t.Errorf("Unexpected response, got: %v, want %v", data, "TTTGameServer\n")
+	}
+}
