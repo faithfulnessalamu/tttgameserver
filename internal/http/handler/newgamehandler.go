78b3d808d72b482d29ccbad55bfe82d9126af5fd
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 14:42:28 +0100

    Write websocket message to stdout

--- internal/http/handler/newgamehandler.go
+++ internal/http/handler/newgamehandler.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
+	"github.com/gorilla/websocket"
 	"github.com/patrickmn/go-cache"
 	"github.com/thealamu/tttgameserver/internal/engine"
 )
@@ -11,8 +14,30 @@ import (
 //NewGameHandler handles creating a new game
 func NewGameHandler(db *cache.Cache) http.HandlerFunc {
 	gE := engine.New(db)
+
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		gameID := gE.StartNewGame()
 		log.Printf("handler.NewGame gameID is %s", gameID)
+
+		//Upgrade the connection
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal("handler.NewGame ", err)
+		}
+
+		//deliver the gameID
+		conn.WriteMessage(websocket.TextMessage, []byte(gameID))
+		for {
+			_, p, err := conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println(strings.TrimSpace(string(p)))
+		}
 	}
 }
