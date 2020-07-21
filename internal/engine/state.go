ddb3316382bdff1504b0498221bc27556484c618
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 23:15:25 +0100

    Update game state struct

--- internal/engine/state.go
+++ internal/engine/state.go
@@ -1,4 +1,23 @@
 package engine
 
 //GameState holds the state of an ongoing game
-type GameState struct{}
+type GameState struct {
+	Data   Data         `json:"data"`
+	Board  [3][3]string `json:"board"`
+	Win    bool         `json:"win"`
+	Winner string       `json:"winner"`
+}
+
+//Data holds current game data
+type Data struct {
+	MaxScore int    `json:"maxScore`
+	Playerx  Player `json:"playerx"`
+	Playero  Player `json:"playero"`
+}
+
+//Player represents a connected player
+type Player struct {
+	ID     int  `json:"id"`
+	Score  int  `json:"score"`
+	Active bool `json:"active"`
+}
