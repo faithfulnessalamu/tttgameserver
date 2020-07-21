4a14452bae4019d112aec6eecae2f9ed774a9a7e
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 12:20:18 +0100

    Inject db dependency into each handler

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,18 +1,26 @@
 package engine
 
+import "github.com/patrickmn/go-cache"
+
 //GameEngine handles game logic
-type GameEngine struct {}
+type GameEngine struct {
+	db *cache.Cache
+}
 
 //New returns a new GameEngine
-func New() GameEngine {
-	return GameEngine{}
+func New(db *cache.Cache) GameEngine {
+	return GameEngine{db: db}
 }
 
 //StartNewGame creates a new game and returns a game ID
 func (gE GameEngine) StartNewGame() string {
 	game := newgame()
-	game.id = newGameID()	//generate new game id
+	game.id = newGameID() //generate new game id
 	//save game
-
+	saveGame(game.id, game)
 	return game.id
+}
+
+func saveGame(id string, g game) {
+
 }
--- internal/http/handler/joingamehandler.go
+++ internal/http/handler/joingamehandler.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/patrickmn/go-cache"
 )
 
 //JoinGameHandler handles game joining
-func JoinGameHandler() http.HandlerFunc {
+func JoinGameHandler(db *cache.Cache) http.HandlerFunc {
 	//gE := engine.New()
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Join Game")
