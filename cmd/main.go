6f2e0a5b47621ba31f9feaddf16e28e8e8ef522e
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-21 07:50:07 +0100

    Handle default route

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"time"
 
-	flag "github.com/spf13/pflag"
-	"github.com/thealamu/tttgameserver/internal/http/server"
+	"github.com/gorilla/mux"
 	"github.com/sethvargo/go-signalcontext"
+	flag "github.com/spf13/pflag"
+	"github.com/thealamu/tttgameserver/internal/http/handler"
+	"github.com/thealamu/tttgameserver/internal/http/server"
 )
 
 //flags
@@ -22,17 +24,21 @@ func main() {
 	serverEnv := server.NewServerEnv()
 	serverEnv.Port = port
 
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler.HomeHandler)
+	serverEnv.Handler = router
+
 	ctx, cancel := signalcontext.OnInterrupt()
-	defer cancel()	//call for cleanup
+	defer cancel() //call for cleanup
 
 	go server.Run(ctx, serverEnv)
 
-	<-ctx.Done()	//wait for CTRL+C
+	<-ctx.Done() //wait for CTRL+C
 
 	//stop the server
 	ctx = context.Background()
 	shutdownCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()	//call for cleanup
+	defer cancel() //call for cleanup
 	err := server.Shutdown(shutdownCtx)
 	if err != nil {
 		log.Fatal("main.Main ", err)
