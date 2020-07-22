a633b21c7dd75f1db7b801b86c35d015cabd653b
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-22 23:40:09 +0100

    Use structured loggin in newgame handler

--- internal/http/handler/newgamehandler.go
+++ internal/http/handler/newgamehandler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/patrickmn/go-cache"
 	"github.com/thealamu/tttgameserver/internal/engine"
+	"go.uber.org/zap"
 )
 
 //NewGameHandler handles creating a new game
@@ -15,6 +16,7 @@ type NewGameHandler struct {
 	gE       engine.GameEngine
 	upgrader websocket.Upgrader
 	conn     *websocket.Conn
+	logger   *zap.Logger
 	gameID   string
 	avatar   string
 }
@@ -31,13 +33,27 @@ func GetNewGameHandler(db *cache.Cache) NewGameHandler {
 }
 
 func (nh NewGameHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//init logger
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Println("Can't initialize zap logger: %v", err)
+		return
+	}
+	nh.logger = logger
+	defer nh.logger.Sync()
+
 	gameID := nh.gE.StartNewGame()
-	log.Printf("handler.NewGame gameID is %s", gameID)
+	nh.logger.Info("Started a new game",
+		zap.String("gameID", gameID),
+	)
 
 	//Upgrade the connection
 	conn, err := nh.upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("handler.NewGame ", err)
+		nh.logger.Error("Upgrading to websocket failed",
+			zap.String("gameID", gameID),
+			zap.Error(err),
+		)
 		return
 	}
 	nh.conn = conn
@@ -65,7 +81,10 @@ func (nh NewGameHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		case gameState := <-c:
 			conn.WriteJSON(gameState) //handle dispatch
 		case <-done:
-			log.Println("handler.NewGame player disconnected")
+			nh.logger.Info("Player disconnected",
+				zap.String("gameID", nh.gameID),
+				zap.String("player", nh.avatar),
+			)
 			return
 		}
 	}
@@ -85,13 +104,28 @@ func (nh NewGameHandler) readMoves() chan int {
 			var m engine.Move
 			err = json.Unmarshal(p, &m)
 			if err != nil {
-				log.Printf("handler.readMoves %s", err)
+				nh.logger.Info("Error reading move",
+					zap.String("gameID", nh.gameID),
+					zap.String("player", nh.avatar),
+					zap.String("err", err.Error()),
+				)
 				continue
 			}
+			//log the move
+			nh.logger.Info("Received move",
+				zap.String("gameID", nh.gameID),
+				zap.String("player", nh.avatar),
+				zap.Int("row", m.Row),
+				zap.Int("col", m.Col),
+			)
 			//try to do move
 			err = nh.gE.MakeMove(nh.gameID, nh.avatar, m)
 			if err != nil {
-				log.Printf("handler.readMoves %s", err)
+				nh.logger.Info("Error making move",
+					zap.String("gameID", nh.gameID),
+					zap.String("player", nh.avatar),
+					zap.String("err", err.Error()),
+				)
 				nh.writeString(err.Error())
 			}
 		}
