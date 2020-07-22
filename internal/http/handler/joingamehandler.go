133b4cfaf02243d85274153396e5c8da94896c71
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-22 23:51:08 +0100

    Use structured logging in joingame handler

--- internal/http/handler/joingamehandler.go
+++ internal/http/handler/joingamehandler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/patrickmn/go-cache"
 	"github.com/thealamu/tttgameserver/internal/engine"
+	"go.uber.org/zap"
 )
 
 //JoinGameHandler handles game joining
@@ -15,6 +16,7 @@ type JoinGameHandler struct {
 	gE       engine.GameEngine
 	upgrader websocket.Upgrader
 	conn     *websocket.Conn
+	logger   *zap.Logger
 	gameID   string
 	avatar   string
 }
@@ -31,13 +33,27 @@ func GetJoinGameHandler(db *cache.Cache) JoinGameHandler {
 }
 
 func (jh JoinGameHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//create logger
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Println("Can't initialize zap logger: %v", err)
+		return
+	}
+	jh.logger = logger
+	defer jh.logger.Sync()
+
 	//get gameID from url
 	gameID := r.URL.Query().Get("gameid")
-	log.Printf("handler.JoinGame gameID is %s", gameID)
+	jh.logger.Info("Asking to join a new game",
+		zap.String("gameID", gameID),
+	)
 
 	conn, err := jh.upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("handler.JoinGame ", err)
+		jh.logger.Error("Upgrading to websocket failed",
+			zap.String("gameID", gameID),
+			zap.Error(err),
+		)
 	}
 	jh.conn = conn
 	defer jh.conn.Close()
@@ -46,7 +62,10 @@ func (jh JoinGameHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := make(chan engine.GameState) //game engine should use this channel to send updates
 	player, err := jh.gE.NewPlayer(gameID, c)
 	if err != nil {
-		log.Println(err)
+		jh.logger.Info("Error joining game",
+			zap.String("gameID", jh.gameID),
+			zap.String("err", err.Error()),
+		)
 		jh.writeString(err.Error()) //write error string to conn
 		return                      //we can't go on
 	}
@@ -63,7 +82,10 @@ func (jh JoinGameHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		case gameState := <-c:
 			conn.WriteJSON(gameState) //handle dispatch
 		case <-done:
-			log.Println("handler.JoinGame player disconnected")
+			jh.logger.Info("Player disconnected",
+				zap.String("gameID", jh.gameID),
+				zap.String("player", jh.avatar),
+			)
 			return
 		}
 	}
@@ -83,13 +105,28 @@ func (jh JoinGameHandler) readMoves() chan int {
 			var m engine.Move
 			err = json.Unmarshal(p, &m)
 			if err != nil {
-				log.Printf("handler.readMoves %s", err)
+				jh.logger.Info("Error reading move",
+					zap.String("gameID", jh.gameID),
+					zap.String("player", jh.avatar),
+					zap.String("err", err.Error()),
+				)
 				continue
 			}
+			//log the move
+			jh.logger.Info("Received move",
+				zap.String("gameID", jh.gameID),
+				zap.String("player", jh.avatar),
+				zap.Int("row", m.Row),
+				zap.Int("col", m.Col),
+			)
 			//try to do move
 			err = jh.gE.MakeMove(jh.gameID, jh.avatar, m)
 			if err != nil {
-				log.Printf("handler.readMoves %s", err)
+				jh.logger.Info("Error making move",
+					zap.String("gameID", jh.gameID),
+					zap.String("player", jh.avatar),
+					zap.String("err", err.Error()),
+				)
 				jh.writeString(err.Error())
 			}
 		}
